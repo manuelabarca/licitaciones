bc810815ad4bc410cc40fc7e9e14f1930f08a569
manuelabarca <labarca.manu@gmail.com>
2016-08-17 15:49:22 -0300

    Update licitaciones_js.js

--- licitaciones_js.js
+++ licitaciones_js.js
@@ -5,7 +5,7 @@
 
 var app = angular.module('licitacionesMercado', []);
 
-licitacionesMercado.controller('licitaciones_all', function($scope, $http){
+app.controller('licitaciones_all', function($scope, $http){
     $scope.fechaActual = new Date();
 
     $http.get("http://link/licitaciones.json?fecha="+$scope.fechaActual+"&ticket=EB9BA9FD-B1DA-4C27-B78D-7A095969577B").then(function (res) {
